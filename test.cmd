9c35f6c9a31dce4afd66a15ce9eecb0e72985bab
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2020-04-23 17:07:32 +0900

    buildもrestoreもしないように！

--- test.cmd
+++ test.cmd
@@ -1,3 +1,3 @@
 dotnet build -c Release
 dotnet run -p "StaticFunctionPointerImplementor" --no-build -c Release helper -directory "./"
-dotnet test --no-build --nologo -v m -c Release -f netcoreapp3.1
+dotnet test --no-restore --no-build --nologo -v m -c Release -f netcoreapp3.1
