af5e2529f6be6b3c13f0f02bbad15421613ec027
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2020-05-01 20:49:48 +0900

    全体に公開するほどのAPIではない

--- Utf8Json.Core/JsonSerializeAction.cs
+++
@@ -1,7 +0,0 @@
-// Copyright (c) All contributors. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-namespace Utf8Json
-{
-    public delegate void JsonSerializeAction<T>(ref JsonWriter writer, T value, JsonSerializerOptions options);
-}
