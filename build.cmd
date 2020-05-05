6a45677abbce1dceda8a2732f1f9c34b88109eb7
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2020-05-05 20:48:35 +0900

    ビルドシステムVisual Studio previewに依存しているのでshoganai

--- build.cmd
+++ build.cmd
@@ -1,2 +1,3 @@
-dotnet build -c Release
+REM dotnet build --nologo --verbosity quiet -c Release
+devenv.exe Utf8JsonV2.sln /Build release
 dotnet run -p "StaticFunctionPointerImplementor" --no-build -c Release helper -directory "./"
--- test.cmd
+++ test.cmd
@@ -1,4 +1,5 @@
-dotnet build --nologo --verbosity quiet -c Release
+REM dotnet build --nologo --verbosity quiet -c Release
+devenv.exe Utf8JsonV2.sln /Build release
 IF %ERRORLEVEL% NEQ 0 EXIT 1
 dotnet run -p "StaticFunctionPointerImplementor" --no-build -c Release helper -directory "./"
 dotnet test --no-restore --no-build --nologo --verbosity minimal -c Release -f netcoreapp3.1
