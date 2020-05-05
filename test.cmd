7610738bdcfa797e21c7f98dbe23ea479e71c333
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2020-05-05 16:26:11 +0900

    ビルド失敗時にテストが走らないようにした

--- test.cmd
+++ test.cmd
@@ -1,3 +1,4 @@
-dotnet build -c Release
+dotnet build --nologo --verbosity quiet -c Release
+IF %ERRORLEVEL% NEQ 0 EXIT 1
 dotnet run -p "StaticFunctionPointerImplementor" --no-build -c Release helper -directory "./"
-dotnet test --no-restore --no-build --nologo -v m -c Release -f netcoreapp3.1
+dotnet test --no-restore --no-build --nologo --verbosity minimal -c Release -f netcoreapp3.1
